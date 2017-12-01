3ad58f85df33c681623b661777012f859d753172
Juliette Rapala <jrapala@gmail.com>
2017-12-01 16:04:28 -0600

    Setting up server.js

---
+++ server.js
@@ -0,0 +1,51 @@
+// Eat-Da-Burger! | By Juliette Rapala
+// =====================================================================================
+
+  // Setup  
+  // =====================================================================================
+
+    // Dependencies
+    var express = require("express");
+	var bodyParser = require("body-parser");
+	var exphbs = require('express-handlebars');
+	var methodOverride = require('method-override');
+
+	// Initialize Express
+	var app = express();
+
+	// Port Config
+    var PORT = process.env.PORT || 3000;
+
+	// App Middleware
+	app.use(methodOverride('_method'));
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(bodyParser.json());
+	app.use(express.static(process.cwd() + "/public"));
+
+	// Handlebars Config
+
+	app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+	app.set("view engine", "handlebars");
+
+	// Route Config
+	require('./routes/htmlRoutes')(app);
+	require('./routes/apiRoutes')(app);
+	
+	// MySQL Config
+	var mysql = require("mysql");
+
+	var connection = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "",
+	  database: "burgers_db"
+	});
+
+	// Connect to database
+	connection.connect(function(err) {
+	  if (err) {
+	    console.error("error connecting: " + err.stack);
+	    return;
+	  }
+	  console.log("connected as id " + connection.threadId);
+	});
